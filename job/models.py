e05e48d4178db0e8dff2c392ddc98e9a3a4fe9d3
Ahmed Shafey <shafeyvip@gmail.com>
2023-11-27 23:35:01 +0200

    add job slugify

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 """
@@ -28,6 +29,12 @@ class Job(models.Model):    # table
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     image = models.ImageField(upload_to=image_upload)
+
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job,self).save(*args, **kwargs)
 
 
     def __str__(self):
--- job/views.py
+++ job/views.py
@@ -12,7 +12,7 @@ def job_list(request):
     context = {'jobs' : page_obj }   # template name
     return render(request,'job/job_list.html',context)
 
-def job_detail(request, id):
-    job_detail = Job.objects.get(id=id)
+def job_detail(request, slug):
+    job_detail = Job.objects.get(slug=slug)
     context = {'job': job_detail}
     return render(request,'job/job_detail.html',context)
